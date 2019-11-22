faeb16b3e78a2122264b488ec489e7d4bf3d0ec8
anuragpatro92 <anuragpatro92@gmail.com>
2019-11-21 21:34:46 -0800

    models

---
+++ webApp/backend/model/UserQualification.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../database/dbConfig.js')
+
+module.exports = db.sequelize.define(
+  'user_qualifications',
+  {
+    id: {
+      type: Sequelize.INTEGER(9),
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: Sequelize.INTEGER(9),
+      required: true
+    },
+    university_name: {
+      type: Sequelize.STRING(100),
+      required: true
+    },
+    degree_level: {
+      type: Sequelize.STRING(100),
+      required: true
+    },
+    qualification_name: {
+      type: Sequelize.STRING(100),
+      required: true
+    },
+    start_dt: {
+      type: Sequelize.DATE,
+      required: true
+    },
+    completion_dt: {
+      type: Sequelize.DATE,
+      required: true
+    },
+    created_on: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  },
+  {
+    timestamps: false
+  }
+)
